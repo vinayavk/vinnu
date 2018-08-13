3a218ba404fa70677ba9e93a912857d63513bc25
vinayavk <35094612+vinayavk@users.noreply.github.com>
2018-08-13 18:13:44 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Ideone
+{
+	public static void main (String[] args) 
+	{
+	          int a,i,sum=0;
+	          Scanner input=new Scanner(System.in);
+	          a=input.nextInt();
+	          for(i=1;i<=a;i++)
+	          {
+	          	sum=sum+i;
+	          }
+	          	System.out.println(sum);
+	          
+	    
+	}
+}
