627bdfa9ce45562365ff2a2297560d42d327a514
vinayavk <35094612+vinayavk@users.noreply.github.com>
2018-08-13 18:09:38 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Ideone
+{
+	public static void main (String[] args) 
+	{
+	          int a,i;
+	          Scanner input=new Scanner(System.in);
+	          a=input.nextInt();
+	          for(i=1;i<=a;i++)
+	          {
+	          	System.out.println("Hello");
+	          }
+	          
+	    
+	}
+}
