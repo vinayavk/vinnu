81dcd2c2a2787d52c1e93b9ee94a8e4b5226adfb
vinayavk <35094612+vinayavk@users.noreply.github.com>
2018-08-13 19:40:04 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Ideone
+{
+	public static void main(String args[])
+	{
+		int n,b,r=0,i; boolean flag=false;
+		Scanner input=new Scanner(System.in);
+		n=input.nextInt();
+		b=input.nextInt();
+		for(i=n;i<b;i++)
+		{
+			if(i%2==0)
+			
+				System.out.print(i + " ");
+			
+		}
+		
+            }
+	
+} 
