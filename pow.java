4e0da2fce7be52e146e6dca665b419abe30bf288
vinayavk <35094612+vinayavk@users.noreply.github.com>
2018-08-13 19:08:40 +0530

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Ideone
+{
+	public static void main(String args[])
+	{
+		int a,b,r=1,i;
+		Scanner input=new Scanner(System.in);
+		a=input.nextInt();
+		b=input.nextInt();
+		for(i=0;i<b;i++)
+		{
+			r=r*a;
+		}
+
+                            System.out.println(r);
+              }
+	
+} 
