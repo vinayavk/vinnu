e511c65dceb16906d27c93c2930272e79c0c9af1
vinayavk <35094612+vinayavk@users.noreply.github.com>
2018-08-13 18:04:30 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Ideone
+{
+	public static void main (String[] args) 
+	{
+	          int a;
+	          Scanner input=new Scanner(System.in);
+	          a=input.nextInt();
+	          if (a%4 == 0)
+	          {
+	          	System.out.println("yes");
+	          }
+	          else
+	          {
+	          	System.out.println("no");
+	          }
+	          
+	    
+	}
+}
