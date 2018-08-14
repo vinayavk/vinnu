4091fece64970cb5a8ba3a760d185c2fb4490de3
vinayavk <35094612+vinayavk@users.noreply.github.com>
2018-08-14 19:22:52 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args)
+	{
+		String s;
+		Scanner input=new Scanner(System.in);
+	            s=input.nextLine();
+	            StringBuilder input1=new StringBuilder();
+	            input1.append(s);
+	            input1=input1.reverse();
+	            System.out.println(input1);
+	}
+}
