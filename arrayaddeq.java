2280059dcae278dc16b30d6c9ab8610ca169fc84
vinayavk <35094612+vinayavk@users.noreply.github.com>
2018-09-05 18:58:46 +0530

    Create arrayaddeq.java

---
+++ arrayaddeq.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				
+				
+				
+					if(a[i]+a[j]==0)
+					{
+					System.out.println(a[i]+" "+a[j]);	
+					}
+				
+				
+			}
+		}
+	}
+}
